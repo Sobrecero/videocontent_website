65b42b49813706da9a30d5a303f75b90fff727fa
Sergey <uidq9497@contiwan.com>
2024-12-28 22:48:15 +0100

    FirstTestDone_updated

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ var personalMovieDB = {
 };
 
 var q1 = prompt("Last film you have seen?", ""),
-    a1 = +prompt("Please, rate this film up to 10 points:", "0");
+    a1 = prompt("Please, rate this film up to 10 points:");
     q2 = prompt("Last film you have seen?", ""),
-    a2 = +prompt("Please, rate this film up to 10 points:", "0");
+    a2 = prompt("Please, rate this film up to 10 points:");
 
 personalMovieDB.movies[q1] = a1;
 personalMovieDB.movies[q2] = a2;
 
-console.log(personalMovieDB);r 
+console.log(personalMovieDB);
