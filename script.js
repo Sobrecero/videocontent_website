ec322489e511af0beefa3e57bcc120163f87c549
Sergey <uidq9497@contiwan.com>
2025-01-12 09:03:31 +0100

    12012025

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var numberOfFilms = +prompt("How many movies did you watch?", "");
+'use strict'
 
-var personalMovieDB = {
+/* VARIABLES */
+
+let numberOfFilms;
+let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
@@ -8,12 +11,56 @@ var personalMovieDB = {
   privat: false
 };
 
-var q1 = prompt("Last film you have seen?", ""),
-    a1 = prompt("Please, rate this film up to 10 points:");
-    q2 = prompt("Last film you have seen?", ""),
-    a2 = prompt("Please, rate this film up to 10 points:");
+/* FUNCTIONS */
 
-personalMovieDB.movies[q1] = a1;
-personalMovieDB.movies[q2] = a2;
+function start() { // Ask how many films you have seen
+  numberOfFilms = +prompt("How many movies did you watch?", "");
+  while (isNaN(numberOfFilms) || numberOfFilms <=0 || numberOfFilms == null || numberOfFilms == '') {
+    numberOfFilms = +prompt("How many movies did you watch?", "")
+  }
+}
 
-console.log(personalMovieDB);
+function showMyDB(hidden) { // Show the object in console if not privat
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+function writeYourGenres(genResArray) { //Ask for 3 favorite genres
+  for (let i = 1; i <= 3; i++) {
+    genResArray.push(prompt(`Write you favorite genres N.${i}`));
+  }
+  return 0;
+}
+
+function rememberMyFilms() { // Ask to rate the films
+  for (let i = 0; i < 2; i++) {
+    let filmName = prompt("Last film you have seen?", ""),
+        filmRating = prompt("Please, rate this film up to 10 points:");
+    
+    if (filmName != null && filmRating != null && filmName.length < 50 && filmRating !='' && filmName != '') {
+      personalMovieDB.movies[filmName] = filmRating;
+    }
+  }
+}
+
+function detectPersonalLevel() { //Rate the amount of the film you have recently seen
+  if (numberOfFilms < 10) {
+    console.log("You saw a little films");
+  } else if (numberOfFilms <= 30) {
+    console.log("You are normal viewer");
+  } else if (numberOfFilms > 30) {
+    console.log("You are a movieholic");
+  } else {
+    console.log("You are a movieholic");
+  }
+}
+
+/* MAIN PROGRAM */
+start();
+writeYourGenres(personalMovieDB.genres);
+rememberMyFilms();
+detectPersonalLevel();
+showMyDB(personalMovieDB.privat);
+
+
